cf4ada4c899572d4f7e0d09e3d37865e4f2c463e
Jay McGlothlin <13893140+mcglow2@users.noreply.github.com>
2024-08-31 15:38:16 -0400

    fixed path

--- scripts/provision_controller.sh
+++ scripts/provision_controller.sh
@@ -3,7 +3,8 @@
 echo "provisioning controller" >> /home/vagrant/touch 
 
 #shared keygen for vagrant
-ssh-keygen -b 2048 -t rsa -q -N "" -f /home/vagrant/.ssh/id_rsa
+ssh-keygen -b 2048 -q -N "" -f /home/vagrant/.ssh/id_rsa
+sudo chown vagrant id_rsa*
 cp /home/vagrant/.ssh/id_rsa.pub /vagrant/tmp/id_rsa.controller.pub
 
 
